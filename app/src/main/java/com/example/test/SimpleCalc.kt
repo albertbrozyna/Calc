0557d84a356972e79cb7ee24c435e347ef827aa9
Albert Brozyna <brozynaalbert@gmail.com>
2025-03-13 00:45:25 +0100

    Fixed advanced_calc xml and andded basic logic to simplecalc.

--- app/src/main/java/com/example/test/SimpleCalc.kt
+++ app/src/main/java/com/example/test/SimpleCalc.kt
@@ -1,6 +1,8 @@
 package com.example.test
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,59 @@ class SimpleCalc : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        val sum = 0
+
+        //Fetching buttons
+        val buttonNumbers : Array<Button> by lazy {
+            arrayOf(
+                findViewById(R.id.id_digit_0),
+                findViewById(R.id.id_digit_1),
+                findViewById(R.id.id_digit_2),
+                findViewById(R.id.id_digit_3),
+                findViewById(R.id.id_digit_4),
+                findViewById(R.id.id_digit_5),
+                findViewById(R.id.id_digit_6),
+                findViewById(R.id.id_digit_7),
+                findViewById(R.id.id_digit_8),
+                findViewById(R.id.id_digit_9),
+            )
+        }
+
+        val operatingButtons : Array<Button> by lazy {
+            arrayOf(
+                findViewById(R.id.id_minus),
+                findViewById(R.id.id_plus),
+                findViewById(R.id.id_equals),
+                findViewById(R.id.id_multiply),
+                findViewById(R.id.id_divide),
+                findViewById(R.id.id_bksp),
+                findViewById(R.id.id_change_sign),
+                findViewById(R.id.id_clear),
+                findViewById(R.id.id_dot)
+            )
+        }
+
+
+        val display : TextView by lazy {
+            findViewById(R.id.display)
+        }
+
+        //Adding listeners to numbers
+
+        buttonNumbers.forEach { button -> {
+            button.setOnClickListener {
+                val number = display.text.toString()
+                val newNumber  = if (number == "0")
+                    button.text
+                else number + button.text
+                display.text = newNumber
+            }
+        } }
+
+
+
+
     }
 }
--- app/src/main/java/com/example/test/advanced_calc.kt
+++ app/src/main/java/com/example/test/advanced_calc.kt
@@ -1,38 +1,20 @@
 package com.example.test
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class advanced_calc : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_advanced_calc)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val button = findViewById<Button>(R.id.menu1);
-        val button1 = findViewById<Button>(R.id.menu2);
-        val button2 = findViewById<Button>(R.id.menu3);
-
-        button.setOnClickListener{
-            val intent = Intent(this,SimpleCalc::class.java)
-            startActivity(intent);
-        }
-        button1.setOnClickListener{
-            val intent = Intent(this,advanced_calc::class.java)
-            startActivity(intent);
-        }
-
-
-
     }
 }
