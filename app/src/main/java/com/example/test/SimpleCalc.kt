b7d537227f44078fb904f3c15bf1c5daa6b80b83
Albert Brozyna <brozynaalbert@gmail.com>
2025-03-14 00:44:43 +0100

    Simple calculator works ok.

--- app/src/main/java/com/example/test/SimpleCalc.kt
+++ app/src/main/java/com/example/test/SimpleCalc.kt
@@ -1,7 +1,6 @@
 package com.example.test
 
 import android.os.Bundle
-import android.text.method.ScrollingMovementMethod
 import android.widget.Button
 import android.widget.HorizontalScrollView
 import android.widget.TextView
@@ -12,7 +11,10 @@ import androidx.core.view.WindowInsetsCompat
 
 class SimpleCalc : AppCompatActivity() {
 
+    private val hsv :HorizontalScrollView by lazy { findViewById(R.id.hss1) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_simple_calc)
@@ -22,11 +24,11 @@ class SimpleCalc : AppCompatActivity() {
             insets
         }
 
+        var operator: String
+
         val display: TextView by lazy {
             findViewById(R.id.display)
         }
-
-        var operator = ""
 
         //Fetching buttons
         val buttonNumbers: Array<Button> by lazy {
@@ -70,11 +72,9 @@ class SimpleCalc : AppCompatActivity() {
             findViewById(R.id.id_change_sign)
         }
 
-
         val equals: TextView by lazy {
             findViewById(R.id.id_equals)
         }
-
 
         //Initializing display value
         display.text = "0"
@@ -86,17 +86,18 @@ class SimpleCalc : AppCompatActivity() {
                 val number = display.text.toString()
                 var newNumber = number
                 //If 0 we need to watch out
-                if(button.text == "0" && number.length > 1 &&!isOperator(number.last())){
+                if (button.text == "0" && number.length > 1 && !isOperator(number.last())) {
                     newNumber += '0'
-                }else if(number == "0" && button.text == "0"){
+                } else if (number == "0" && button.text == "0") {
                     newNumber = "0"
-                }else if(number == "0"){
+                } else if (number == "0") {
                     newNumber = button.text.toString()
-                }else{
+                } else {
                     newNumber += button.text
                 }
 
                 display.text = newNumber
+                updateScreen()
             }
         }
 
@@ -104,8 +105,10 @@ class SimpleCalc : AppCompatActivity() {
 
         dot.setOnClickListener {
             val number = display.text.toString()
-            val newNumber = if (number.last() != '.' && !isNumberContainDot(number) && !isOperator(number.last())) number + "." else number
+            val newNumber =
+                if (number.last() != '.' && !isNumberContainDot(number) && !isOperator(number.last())) "$number." else number
             display.text = newNumber
+            updateScreen()
         }
 
         //Listener for backspace
@@ -113,7 +116,7 @@ class SimpleCalc : AppCompatActivity() {
             val number = display.text.toString()
             var newNumber = number.substring(0, number.length - 1)
 
-            if(number.length == 2 && number.first() == '-'){
+            if (number.length == 2 && number.first() == '-') {
                 newNumber = "0"
 
             }
@@ -133,153 +136,158 @@ class SimpleCalc : AppCompatActivity() {
         //Listener for change sign
         changeSign.setOnClickListener {
             val number = display.text.toString()
-            var newNumber = "0"
+            var newNumber: String
 
             var operatorIndex = -1
             //Checking if number already contains two digits
-            if(containOperatorAndTwoDigits(number)){
+            if (containOperatorAndTwoDigits(number)) {
                 operatorIndex = findOperatorIndex(number)
             }
 
             //If first char is minuis then we need to add one to index
             //Checking at what number we should change -
 
-            if(operatorIndex == -1){
+            if (operatorIndex == -1) {
                 //It is first number
                 //Checking if number already contains minus
-                if(number.first() == '-'){
-                    newNumber = number.substring(1,number.length)
-                }else{
-                    newNumber = '-' + number
+                newNumber = if (number.first() == '-') {
+                    number.substring(1, number.length)
+                } else {
+                    "-$number"
                 }
 
-            }else{
+            } else {
                 //It is second number
                 //Checking if number already contains minus
 
-                if(number[operatorIndex + 1] == '-'){
+                newNumber = if (number[operatorIndex + 1] == '-') {
                     //Change for plus
-                    newNumber = number.substring(0,operatorIndex + 1) + number.substring(operatorIndex + 2)
-                }else{
-                    newNumber = number.substring(0,operatorIndex + 1) + '-' + number.substring(operatorIndex + 1)
+                    number.substring(0, operatorIndex + 1) + number.substring(operatorIndex + 2)
+                } else {
+                    number.substring(
+                        0,
+                        operatorIndex + 1
+                    ) + '-' + number.substring(operatorIndex + 1)
                 }
             }
-            if(number == "0"){
+            if (number == "0") {
                 newNumber = "0"
             }
 
             display.text = newNumber
+            updateScreen()
         }
 
         //Listener for equals
         equals.setOnClickListener {
-        var number = display.text.toString()
-        var newNumber = number
+            val number = display.text.toString()
+            var newNumber = number
 
-        if (containOperatorAndTwoDigits(number)) {
-            newNumber = calculateExpression(number)
+            if (containOperatorAndTwoDigits(number)) {
+                newNumber = calculateExpression(number)
+            }
+
+            display.text = newNumber
+            updateScreen()
         }
-
-        display.text = newNumber
-    }
 
 
         //Listeners for operating buttons
-        operatingButtons.forEach{
-            button ->
-                button.setOnClickListener{
-                    var number = display.text.toString()
-                    var newNumber = number
+        operatingButtons.forEach { button ->
+            button.setOnClickListener {
+                val number = display.text.toString()
+                var newNumber = number
 
-                    if(containOperatorAndTwoDigits(number)){
-                        newNumber = calculateExpression(number)
-                    }
-
-                    operator = button.text.toString()
-                    //If the operator is last char then replace operator
-                    if(isOperator(newNumber.last())){
-                        newNumber = newNumber.dropLast(1) + operator
-                    }else{
-                        newNumber += operator
-                    }
-
-                    display.text = newNumber
+                if (containOperatorAndTwoDigits(number)) {
+                    newNumber = calculateExpression(number)
                 }
+
+                operator = button.text.toString()
+                //If the operator is last char then replace operator
+                if (isOperator(newNumber.last())) {
+                    newNumber = newNumber.dropLast(1) + operator
+                } else {
+                    newNumber += operator
+                }
+
+                display.text = newNumber
+                updateScreen()
+            }
         }
 
 
     }
 
-    fun findOperatorIndex(s : String) : Int{
+    fun findOperatorIndex(s: String): Int {
         //Founding operator index
         var operatorIndex = 0
         var add = 0
         //If minus is first
-        if(s.first() == '-'){
+        if (s.first() == '-') {
             add = 1
         }
 
-        val s_new =  if(s.first() == '-') s.substring(1,s.length) else s
+        val sNew = if (s.first() == '-') s.substring(1, s.length) else s
 
         var found = false
-        for(c in s_new){
-            if(isOperator(c)){
+        for (c in sNew) {
+            if (isOperator(c)) {
                 found = true
                 break
             }
             operatorIndex++
         }
 
-        if(!found){
-            return -1;  //Index not found
+        if (!found) {
+            return -1 //Index not found
         }
         return operatorIndex + add
     }
 
 
-    fun isNumberContainDot(s :String ):Boolean{
+    fun isNumberContainDot(s: String): Boolean {
 
         val operatorIndex = findOperatorIndex(s)
 
-        if(operatorIndex == -1){
+        if (operatorIndex == -1) {
             //It is first number
             return s.contains('.')
-        }else{
+        } else {
             //It is second number
-            val subs = s.substring(operatorIndex,s.length)
+            val subs = s.substring(operatorIndex, s.length)
 
             return subs.contains('.')
         }
     }
 
 
-    fun isOperator(c : Char ) : Boolean{
+    fun isOperator(c: Char): Boolean {
         return c == '+' || c == '-' || c == '/' || c == '*'
     }
 
-    fun containOperatorAndTwoDigits(s : String) : Boolean{
+    fun containOperatorAndTwoDigits(s: String): Boolean {
         //Finding operator index
         val operatorIndex = findOperatorIndex(s)
 
         //No operator, return false
-        if(operatorIndex == -1){
+        if (operatorIndex == -1) {
             return false
         }
 
         //Checking if operator is not last element
-        if(!isOperator(s.last() ) && s.last() != '.'){
+        if (!isOperator(s.last()) && s.last() != '.') {
             return true
         }
 
         return false
     }
 
-    fun calculateExpression(e : String) : String{
+    fun calculateExpression(e: String): String {
         val operatorIndex = findOperatorIndex(e)
 
         //Splitting numbers by this index
 
-        val firstNumber = e.substring(0,operatorIndex)
+        val firstNumber = e.substring(0, operatorIndex)
         val operator = e[operatorIndex]
         val secondNumber = e.substring(operatorIndex + 1)
 
@@ -289,7 +297,7 @@ class SimpleCalc : AppCompatActivity() {
         if (operator == '/' && secondNumberDb == 0.0) return "Error"
 
         //Calculating
-        when (operator){
+        when (operator) {
             '+' -> firstNumberDb += secondNumberDb
             '-' -> firstNumberDb -= secondNumberDb
             '*' -> firstNumberDb *= secondNumberDb
@@ -297,23 +305,28 @@ class SimpleCalc : AppCompatActivity() {
         }
 
         //Deleting zeros from the end and dot if they are
-        var sum_str = firstNumberDb.toString()
-        if(sum_str.length > 1 || sum_str.first() != '0'){
-            while (sum_str.last() == '0'){
-                sum_str = sum_str.substring(0,sum_str.length - 1)
+        var sumStr = firstNumberDb.toString()
+        if (sumStr.length > 1 || sumStr.first() != '0') {
+            while (sumStr.last() == '0') {
+                sumStr = sumStr.substring(0, sumStr.length - 1)
             }
         }
 
         //Delete the dot
-        if(sum_str.last() == '.'){
-            sum_str = sum_str.substring(0,sum_str.length - 1)
+        if (sumStr.last() == '.') {
+            sumStr = sumStr.substring(0, sumStr.length - 1)
         }
 
-        if(sum_str == "-0"){
-            sum_str = "0"
+        if (sumStr == "-0") {
+            sumStr = "0"
         }
 
-        return sum_str
+        return sumStr
     }
 
+    fun updateScreen() {
+        hsv.post {
+            hsv.fullScroll(HorizontalScrollView.FOCUS_RIGHT)
+        }
+    }
 }
